e1eab53ee2b23f550e7fc3e20c4ecb3ce1674118
Amir <amirhasantalebi1400@gmail.com>
2024-05-02 22:51:22 +0330

    Add static

--- loginOrSignUp/settings.py
+++ loginOrSignUp/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/medias/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
